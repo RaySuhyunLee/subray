347ed92a673949730d2da52793c31b0f351f64a4
Ray Suhyun Lee <raysuhyunlee@gmail.com>
2022-03-06 23:26:11 +0900

    Update payload schema

--- server/schemas/public_api.py
+++ server/schemas/public_api.py
@@ -1,32 +1,22 @@
-from datetime import date, datetime
+from datetime import datetime
 from enum import Enum
 
 from pydantic import BaseModel
 
 
-class SubwayDirection(Enum):
-    UP_OR_OUTER_CIRCLE = 0
-    DOWN_OR_INNER_CIRCLE = 1
+class SeoulSubwayTrainStatus(Enum):
+    ARRIVING = 0
+    ARRIVED = 1
+    DEPARTED = 2
 
 
-class SeoulSubwayLocationPayload(BaseModel):
-    subwayId: str
+class SeoulSubwayArrivalPayload(BaseModel):
     subwayNm: str  # readable line name (ex. 2호선)
     statnId: str  # station Id
     statnNm: str  # station name
     trainNo: str
-    lastRecptnDt: date  # last update date
     recptnDt: datetime  # last update time
-    updnLine: SubwayDirection
-    statnTid: str  # last station id
-    statnTnm: str  # last station name
-    directAt: int  # 0: normal, 1: express
-    lstcarAt: int  # 0: normal, 1: last train
+    trainSttus: SeoulSubwayTrainStatus
 
-    @property
-    def is_express(self) -> bool:
-        return self.directAt == 1
-
-    @property
-    def is_last_train(self) -> bool:
-        return self.lstcarAt == 1
+    class Config:
+        extra = 'ignore'
